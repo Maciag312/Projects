8ebdc491b47aea1bf5880cf192918ece7138479f
Maciag312 <46439378+Maciag312@users.noreply.github.com>
2020-03-06 08:55:57 +0100

    Update iot3.cpp

--- iot3.cpp
+++ iot3.cpp
@@ -1,4 +1,4 @@
-#define LED_PIN 13
+#define LED_PIN 6
 #define BUTTON_BLUE_PIN 2
 #define BUTTON_RED_PIN 3
 
@@ -8,44 +8,57 @@ bool lastBtnBlueState = LOW;
 bool lastBtnRedState = LOW; 
 String wiadomosc = "";
 bool turnlight = false;
+bool isBlinked = false;
 unsigned long timeStart = 0;
 unsigned long towait = 0;
 
 void setup()
 {
-  	Serial.begin(9600);
-  	pinMode(BUTTON_BLUE_PIN, INPUT_PULLUP); //przycisk jako wejście
-  	Serial.println("Wcisnij przycisk!");
-  	pinMode(LED_PIN, OUTPUT);
+    Serial.begin(9600);
+    pinMode(BUTTON_BLUE_PIN, INPUT_PULLUP); //przycisk jako wejście
+    Serial.println("Give message!");
+    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-  if(turnlight){
+  if(turnlight==true){
     digitalWrite(LED_PIN, HIGH);
   }else{
     digitalWrite(LED_PIN, LOW);
   }
   
-  if(((millis()-timeStart)>towait)&&towait!=0){
+  if(((millis()-timeStart)>towait)&&towait!=0&&isBlinked){
     turnlight = false;
+    isBlinked = false;
   }
   if(Serial.available() > 0) { //Czy Arduino odebrało dane
     wiadomosc = Serial.readStringUntil('\n'); 
-    if(wiadomosc=="LED ON"){
+        Serial.println("message is recieved!");
+        Serial.println(wiadomosc);
+
+    if(wiadomosc == "LED ON"){
       turnlight = true;
-    }else if(wiadomosc=="LED OFF"){
+      wiadomosc = "";
+    }else if(wiadomosc == "LED OFF"){
       turnlight = false; 
-    }else if(wiadomosc.substring(0,9)=="LED BLINK"){
+      wiadomosc = "";
+    }else if(wiadomosc.substring(0,9) == "LED BLINK"){
+              
       if(wiadomosc.length()==9){
            turnlight = true;
+           isBlinked = true;
+          timeStart = millis();
       }
-      else if(wiadomosc.charAt(11)=='<'&&wiadomosc.charAt(wiadomosc.length()-1)=='>'){
-         String numb = wiadomosc.substring(12,wiadomosc.length()-2);
-        	timeStart = millis();
-        	towait = numb.toInt();
-        	turnlight = true; 
+      else if(wiadomosc.charAt(10) == '<' && wiadomosc.charAt(wiadomosc.length()-1) == '>'){
+         
+         String numb = wiadomosc.substring(11,wiadomosc.length()-1);
+          timeStart = millis();
+          towait = numb.toInt();
+          turnlight = true; 
+          isBlinked = true;
       }
+      wiadomosc = "";
     }
   }  
 
